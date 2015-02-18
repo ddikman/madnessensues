0bec7fe4ab2d41f5f2656308075f7d0c5ce6eb54
David Dikman <ddikman@gmail.com>
2015-02-18 01:54:04 +0100

    Tried to change page redirect url

--- app.js
+++ app.js
@@ -21,13 +21,9 @@ app.get('/api/page/:page_name', function(req, res){
 
 	var success = pageExists(req.params.page_name);
 
-	var responseHost = req.hostname;
-	if(req.port == undefined || req.port != 80)
-		responseHost = responseHost + ":" + req.port;
-
 	res.json({
 		success: success,
-		url: responseHost + "/" + req.params.page_name
+		url: req.protocol + "://" + req.get("host") + "/" + req.params.page_name
 	});
 
 });
